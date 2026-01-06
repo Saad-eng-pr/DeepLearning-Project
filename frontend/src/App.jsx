85468352a6a599805c57f7c5cde6153f04188a2a
saada <saad.amal@etu.uca.fr>
2026-01-06 21:11:11 +0100

    Premier version du frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import SideBar from './components/SideBar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import ChatBox from './components/ChatBox'
 import Login from './pages/login'
 import Loading from './pages/Loading'
 import './assets/prism.css'
 
 const App = () => {
+
+  const {pathname} = useLocation()
+
+  if(pathname === '/loading') return <Loading />
   return (
     <>
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
         <div className='flex h-screen w-screen'>
           <SideBar />
 
           <Routes>
-            <Route path="/" element={<ChatBox />} />
-            
-            <Route path="/login" element={<Login />} />
-          
-
-            
+            <Route path="/" element={<ChatBox />} />          
           </Routes>
         </div>
 
--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets';
 import Message from './Message';
 
 
 const ChatBox = () => {
+  const containerRef = useRef(null);  
+
   const {selectedChat, theme} = useAppContext();
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -22,10 +24,18 @@ const ChatBox = () => {
     }
   }, [selectedChat])
 
+  useEffect(() => {
+    containerRef.current?.scrollTo({
+      top: containerRef.current.scrollHeight,
+      behavior: "smooth",
+    })
+  }, messages)
+  
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl-pr-40'>
+      
       {/* Chat messages */}
-      <div className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
         {messages.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
             <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark}  alt="logo" className='w-full max-w-56 sm:max-w-68'/>
